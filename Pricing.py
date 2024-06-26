4936d1f67b7c0f743d6e8ef87b8e15433c2cd571
Bobby Beemer <bobbymbeemer@gmail.com>
2024-06-25 23:21:19 -0400

    Update Pricing.py with DVOB

--- Pricing.py
+++ Pricing.py
@@ -40,3 +40,30 @@ def put_price(spot_price, strike_price, compound_rate, time_to_maturity, stdev):
     d1 = (math.log(spot_price/strike_price) + time_to_maturity*(compound_rate + stdev**2/2)) / (stdev * math.sqrt(time_to_maturity))
     d2 = d1 - (stdev * math.sqrt(time_to_maturity))
     return norm().cdf(-d2)*(strike_price/math.exp(compound_rate*time_to_maturity)) - norm().cdf(-d1)*spot_price
+
+
+# assumes all rates move together
+def dollar_value_bp(par, coupon_rate, term, zero_rates):
+    # get payments associated with the bond
+    payments = [par*coupon_rate]*(term-1) + [par*coupon_rate+par]
+    # create running totals of the $dur and $conv
+    doll_dur = 0
+    doll_conv = 0
+    DELTA_BP = 0.01
+    # for every payment, calculate that payments $dur and $conv and add it to the total
+    for i in range(0, len(payments)):
+        doll_dur += dollar_duration(payments[i], zero_rates[i], i+1)
+        doll_conv += dollar_convexity(payments[i], zero_rates[i], i+1)
+    
+    # taylor series approximation for change in value of bond
+    return (doll_dur * DELTA_BP) + (1/2 * doll_conv * DELTA_BP**2)
+
+
+# helper functions
+# first derivative of pricing function with respect to r
+def dollar_duration(payment, rate, term):
+    return (payment * -term) / (1+rate)**(term+1)
+
+# second derivative of pricing function with respect to r
+def dollar_convexity(payment, rate, term):
+    return (payment * (term**2 + term)) / (1+rate)**(term+2)
